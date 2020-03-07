f6da422044c2d6f1513e19481acf2a2793bc2462
Dan Dismuke <dandismuke@gmail.com>
2020-03-06 23:37:53 -0800

    Working with local mongo and indexeddb.

---
+++ public/service-worker.js
@@ -0,0 +1,72 @@
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/favicon.ico',
+  '/manifest.webmanifest',
+  '/styles.css'
+];
+
+const PRECACHE = "pre-cache";
+
+// install
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(PRECACHE).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// activate
+/* self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== PRECACHE && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+}); */
+
+// fetch
+self.addEventListener("fetch", function(e) {
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(e.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(e.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            console.log('No internet. Retrieve from cache...');
+            // Network request failed, try to get it from the cache.
+            return cache.match(e.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  e.respondWith(
+    caches.open(PRECACHE).then(cache => {
+      return cache.match(e.request).then(response => {
+        return response || fetch(e.request);
+      });
+    })
+  );
+});
